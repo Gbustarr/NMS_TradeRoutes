a41d2f55491e660768885f9112c7cf84fb31d6c6
Guillermo <gbustamante21@alumnos.utalca.cl>
2023-01-24 02:49:23 -0300

    .

---
+++ screens/Home.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const Home = ()=>{
+
+    return (
+        <View>
+            <Text>ti</Text>
+            <StatusBar style='auto'/>
+        </View>
+    )
+
+};
+
+export default Home;
